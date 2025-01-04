0a40901350d33c376791c3075d72d8a10658fab5
Solractys <carloseduardosilvasouzadelima@gmail.com>
2025-01-04 00:59:58 -0300

    initial commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,19 +1,16 @@
-import { Fullscreen } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
 const AuthenticationPage = () => {
   return (
     <main className="h-screen grid grid-cols-2 relative">
-      <div className="">
-        <Image
-          alt="background"
-          src="/AuthImg.jpeg"
-          fill
-          className="w-full h-full"
-        />
+      <div className="relative w-full flex flex-col bg-muted p-10 text-primary-foreground">
+        <div className="w-full h-[30%] bg-gradient-to-t from-transparent to-black/50 absolute top-0 left-0 z-10">
+          <div className="w-full h-[40%] bg-gradient-to-b from-transparent to-black/50 absolute bottom-0 left-0 z-10">
+            <Image src="/AuthImg.jpeg" alt="login image" fill className="w-full h-full object-cover"/>
+          </div>
+        </div>
       </div>
-      <div>Login</div>
     </main>
   );
 };
